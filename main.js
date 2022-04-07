7654972369727d243ce8b09eab5d723ae02fbc3a
katynguyen96 <56082798+katynguyen96@users.noreply.github.com>
2022-04-07 11:06:37 +0700

    create example for js training

---
+++ main.js
@@ -0,0 +1,14 @@
+// variable
+let a;
+const b = 0;
+var c;
+
+const three = 1 + 2;
+let four = 2*2;
+let five = 15/3;
+const two = 2**1;
+const result = 20%5;
+console.log(typeof(three));
+const strings = "string";
+const z = null;
+const t = undefined;
