df25022440b132ac20469be4351510f3022d2047
katynguyen96 <56082798+katynguyen96@users.noreply.github.com>
2022-04-19 17:28:39 +0700

    practice example

--- promise/promise.js
+++ promise/promise.js
@@ -1,5 +1,5 @@
 const promise = new Promise((resolve, reject)=>{
-	if(true)
+	if(false)
 		resolve("everything fine");
 	else reject('somthing wrong');
 })
@@ -20,3 +20,31 @@ Promise.all([promise,promise2,promise3,promise4])
 .then(value=>{
 	console.log(value);
 });
+
+promise
+	.then(result=>result + '!')
+	.then(result2=>result2+'?')
+	.catch(()=>console.log("Error"))
+	.then((result3)=>{
+		console.log(result3+'!');
+		return result3+'!';
+	})
+	.finally((result4)=>{
+		console.log(result4 + "+")
+		return result4 + "-";
+		console.log(result4)
+	});
+
+const urls = [
+	'https://jsonplaceholder.typicode.com/posts',
+	'https://jsonplaceholder.typicode.com/albums',
+	'https://jsonplaceholder.typicode.com/users'
+];
+
+Promise.all(urls.map(url=>{
+	return fetch(url).then(resp=>resp.json())
+})).then(results=>{
+	console.log(results[0]);
+	console.log(results[1]);
+	console.log(results[2]);
+}).catch(()=>console.log("Can't get data"));
